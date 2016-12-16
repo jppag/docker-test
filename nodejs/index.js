8944f35d23d14950c4c71439d693befef3157a2c
JP Pagnoncelli <jppagnoncelli@JP-Pagnoncelli.local>
2016-12-16 10:29:01 -0200

    Initial commit: Everything running

---
+++ nodejs/index.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Welcome to Node.js!\n");
+});
+
+// Listen on port 3000, IP defaults to 127.0.0.1
+server.listen(3000);
+
+// Put a friendly message on the terminal
+console.log("Server running at http://127.0.0.1:3000/");
